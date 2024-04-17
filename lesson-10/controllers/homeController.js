fd38a7a8cfea2de22ad3bfd6d2a96300c4a25bab
sshoon <162391329+sonshoon@users.noreply.github.com>
2024-04-17 01:28:21 +0000

    10 11끝

--- lesson-10/controllers/homeController.js
+++ lesson-10/controllers/homeController.js
@@ -11,4 +11,21 @@ exports.respondWithName; // @TODO: 사용자 정의 EJS 뷰를 사용한 응답
  * Listing 10.3 (p. 160)
  * 뷰로의 라우트 매개변수 전달
  */
-exports.respondWithName2; // @TODO: 요청 매개변수로 지역 변수 할당. 그다음 렌더링된 뷰로 지역 변수 전달.
+exports.respondWithName = (req,res) =>   {
+    let paramsName =req.params.myName
+    res.render('index', {
+        name:paramsName,
+        people:{ 
+            first:{
+                name:"mary",
+                city:"jeonju"
+            },
+            second:{
+                name:"ju",
+                city:"ko"
+            }
+        }
+    });
+}; // @TODO: 요청 매개변수로 지역 변수 할당. 그다음 렌더링된 뷰로 지역 변수 전달.
+
+exports.gethome;
--- lesson-11/controllers/homeController.js
+++ lesson-11/controllers/homeController.js
@@ -2,16 +2,30 @@
 "use strict";
 
 /**
- * Listing 10.2
+ * Listing 10.2 (p. 159)
+ * 컨트롤러 동작에 의한 뷰 렌더링
  */
-exports.respondWithName = (req, res) => {
-    res.render('index');
-}
+exports.respondWithName; // @TODO: 사용자 정의 EJS 뷰를 사용한 응답
 
 /**
- * Listing 10.3
+ * Listing 10.3 (p. 160)
+ * 뷰로의 라우트 매개변수 전달
  */
-exports.respondWithName2 = (req, res) => {
-    let paramsName = req.params.myName;
-    res.render('index', { name: paramsName });
-};
+exports.respondWithName = (req,res) =>   {
+    let paramsName =req.params.myName
+    res.render('index', {
+        name:paramsName,
+        people:{ 
+            first:{
+                name:"mary",
+                city:"jeonju"
+            },
+            second:{
+                name:"ju",
+                city:"ko"
+            }
+        }
+    });
+}; // @TODO: 요청 매개변수로 지역 변수 할당. 그다음 렌더링된 뷰로 지역 변수 전달.
+
+exports.gethome;
